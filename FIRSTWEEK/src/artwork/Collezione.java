b411557aee3c72e63544ac1bfe250888475e69c3
Asius22 <76838719+Asius22@users.noreply.github.com>
2022-03-26 13:46:24 +0100

    scritta classe Collezione completa dei metodo add, prinCollection, printEcumbrace

--- FIRSTWEEK/src/artwork/Collezione.java
+++ FIRSTWEEK/src/artwork/Collezione.java
@@ -1,7 +1,54 @@
 package artwork;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collezione {
-    private String nome;
+    private String name;
     private List<ArtWork> collezione;
 
+    public Collezione(String name) {
+        this.name = name;
+        this.collezione = new ArrayList<>();
+    }
+
+    public Collezione(String name, List<ArtWork> collezione) {
+        this.name = name;
+        this.collezione = collezione;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ArtWork> getCollezione() {
+        return collezione;
+    }
+
+    public void setCollezione(List<ArtWork> collezione) {
+        this.collezione = collezione;
+    }
+
+    public boolean add(ArtWork a) {
+        if (!collezione.contains(a))
+            return collezione.add(a);
+        return false;
+    }
+
+    public String printCollection() {
+        StringBuilder res = new StringBuilder();
+        for (ArtWork a : collezione)
+            res.append(a.toString())
+                    .append("\n");
+
+        return res.toString();
+    }
+
+    public double princEncumbrace (ArtWork a){
+        return a.printEncumbrace();
+    }
 }
