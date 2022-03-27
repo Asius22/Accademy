950d973642af093bcdeee3ebe9c2d5688c9fa37b
Asius22 <76838719+Asius22@users.noreply.github.com>
2022-03-27 13:45:51 +0200

    testati metodi statMedico e listaMedico

--- FIRSTWEEK/src/azienda_sanitaria/Test.java
+++ FIRSTWEEK/src/azienda_sanitaria/Test.java
@@ -1,2 +1,24 @@
-package azienda_sanitaria;public class Test {
+package azienda_sanitaria;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Paziente> pazienti = new ArrayList<>();
+        ArrayList<Medico> medici = new ArrayList<>();
+        pazienti.add(new Paziente("ciro", 2));
+        pazienti.add(new Paziente("vincenzo", 1));
+        pazienti.add(new Paziente("CIRO", 3));
+        medici.add(new Medico("ciro"));
+        medici.add(new Medico("Vincenzo"));
+
+        AziendaSanitaria azienda = new AziendaSanitaria();
+        azienda.setMedici(medici);
+        for (Paziente p : pazienti)
+            azienda.aggPaziente(p);
+        System.out.println(azienda.listaMedico(medici.get(0)));
+        System.out.println(azienda.statMedico());
+    }
+
 }
+
